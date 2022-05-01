cedcbe2d0a092f5d44ef7bc1a3b12912421c1a18
Danny Engelman <danny@engelman.nl>
2022-05-01 11:19:56 +0200

    initial commit

---
+++ element.js
@@ -0,0 +1,21 @@
+// read dev.to API, list user="..." articles
+customElements.define( "dev-to-posts",
+class extends HTMLElement {
+async connectedCallback(
+    user = this.getAttribute("user"),
+    uri = `//dev.to/api/articles?state=all&username=${user}`,
+) {
+    let json = await (await fetch(uri, {mode: "cors"})).json();
+    this.innerHTML = 
+        `<div style="font:15px arial;background:beige">` +
+        `<b>${user} DEV.to posts:</b>` +
+            (json.map(
+            ({title, readable_publish_date, url, public_reactions_count}) => (
+                `<div style="display:grid;grid:1fr/60px 60px 1fr">` +
+                `<span>${readable_publish_date}</span>`+
+                `<span>⭐ ${public_reactions_count}</span>` +
+                `<a href="${url}" target="_blank">${title.split("(")[0]}</a>` +
+                `</div>`
+            )).join``) + 
+        "</div>";
+}});
